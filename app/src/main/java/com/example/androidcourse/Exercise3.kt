1e33c56df9664b862af38d190d428ce8170c4ec7
Evgenii Koduhov <evg_kod@mail.ru>
2022-09-16 14:46:06 +0700

    Exercise 3 comleted

--- app/src/main/java/com/example/androidcourse/Exercise3.kt
+++ app/src/main/java/com/example/androidcourse/Exercise3.kt
@@ -1,8 +1,18 @@
 package com.example.androidcourse
+import android.util.Log
 
 class Exercise3 {
     fun run(){
-
+        Log.i(null, "")
+        Log.i(null, "Exercise tree, list")
+        val numbers = mutableListOf<Int>()
+        for(i in 1..10){
+            numbers += i
+        }
+        logCollection(numbers)
     }
-    
+
+    private fun logCollection(inputList: List<Int>){
+        for(item in inputList) Log.i(null, item.toString())
+    }
 }
