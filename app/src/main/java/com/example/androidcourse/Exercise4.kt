e4a95cabf635546758668a091442a8c0eccfb55e
Evgenii Koduhov <evg_kod@mail.ru>
2022-09-21 20:03:53 +0700

    extra property for a class

--- app/src/main/java/com/example/androidcourse/Exercise4.kt
+++ app/src/main/java/com/example/androidcourse/Exercise4.kt
@@ -6,18 +6,22 @@ class Exercise4 {
 
     fun run(){
         Log.i(null, "")
-        Log.i(null, "Exercise four, OOP")
+        println("Exercise four, OOP")
         val animals = mutableListOf<Animal>()
 
         animals.add(Herbivore("Giraffe"))
         animals.add(Herbivore("Antelope"))
-        animals.add(Predator("Wolf"))
-        animals.add(Predator("Crocodile", false));
+        animals.add(Predator("Wolf", 200f))
+        animals.add(Predator("Crocodile", 300f, false))
+
 
         for (animal in animals){
 
             val fur = if (animal.hasFur) "has fur" else "doesn't have fur"
-            Log.i(null, "This is a(an) ${animal.name} of type ${animal::class.simpleName} it $fur and its tastes are: ${animal.askTastePreference()}")
+            println("This is a(an) ${animal.name} of type ${animal::class.simpleName} it $fur and its tastes are: ${animal.askTastePreference()}")
+            if (animal is Predator){
+                println("This predator's weight is ${animal.weight}")
+            }
         }
         Log.i(null, "")
 
@@ -30,13 +34,15 @@ class Exercise4 {
     }
 
     class Herbivore(name: String, hasFur: Boolean = true) : Animal(name, hasFur){
+
         override fun askTastePreference(): String {
             return "I only eat grass and vegetables"
         }
 
     }
 
-    class Predator(name: String, hasFur: Boolean = true) : Animal(name, hasFur){
+    class Predator(name: String, val weight: Float, hasFur: Boolean = true) : Animal(name, hasFur){
+
         override fun askTastePreference(): String {
             return "I prefer meat"
         }
